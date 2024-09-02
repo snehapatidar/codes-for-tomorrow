b7d36c4bc4516eb6482f42276c82c0b27d8761c6
Sneha Patidar <144313362+snehapatidar@users.noreply.github.com>
2024-09-02 19:13:48 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
     
 
     
-    useEffect(() => {
+ useEffect(() => {
      async function fetchdata(){
         const response = await fetch('https://jsonplaceholder.typicode.com/posts')
         const jsondata = await response.json();
